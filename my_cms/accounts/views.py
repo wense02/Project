5e787ab4715eeb1a5ae31267aa8648012d1feaf6
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-07-25 17:48:14 +0100

    Data Render - [x] Query data from database - [x] Render data to template

--- my_cms/accounts/views.py
+++ my_cms/accounts/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    websites = Website.objects.all()
+    customers = Customer.objects.all()
+    total_websites = websites.filter(status='Hosted').count()
+    total_templates = Template.objects.all().count()
+    context = {'websites': websites, 'customers': customers, 'total_websites': total_websites, 'total_templates': total_templates}
+    return render(request, 'accounts/dashboard.html', context)
 
 def templates(request):
-    return render(request, 'accounts/templates.html')
+    templates = Template.objects.all()
+    return render(request, 'accounts/templates.html', {'templates': templates})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
