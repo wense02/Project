3c73c075622e21cffc5989b1b948c0def2c57418
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-07-25 17:12:40 +0100

    Database Relationship Setup - [x] Add many to many relationship - [x] Store data in database

--- my_cms/accounts/admin.py
+++ my_cms/accounts/admin.py
@@ -3,4 +3,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Template)
+admin.site.register(Tag)
 admin.site.register(Website)
--- my_cms/accounts/models.py
+++ my_cms/accounts/models.py
@@ -11,19 +11,23 @@ class Customer (models.Model):
     def __str__(self):
         return self.name
 
-class Template(models.Model):
-    CATEGORY = (
-        ('porfolio', 'porfolio'),
-        ('ecommerce', 'ecommerce'),
-        ('education', 'education'),
-        ('health', 'health')
-    )
+class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
-    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+
+    def __str__(self):
+        return self.name
+
+class Template(models.Model):
+    name = models.CharField(max_length=200, null=True)
     views = models.CharField(max_length=200, null=True)
     pages = models.CharField(max_length=200, null=True)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
 
 class Website(models.Model):
     STATUS = (
@@ -31,9 +35,12 @@ class Website(models.Model):
         ('Completed', 'Completed'),
         ('Hosted', 'Hosted')
     )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    template = models.ForeignKey(Template, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+
 
 
     
