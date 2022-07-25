8b9e342c7b3e9a2032463f0bdf55c1fb20cafee8
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-07-25 16:01:26 +0100

    Models Setup - [x] Create Models (Customer, Template, Website) - [x] Make migrations - [x] Register models on admin site - [x] Add first customer

--- my_cms/accounts/models.py
+++ my_cms/accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer (models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Template(models.Model):
+    CATEGORY = (
+        ('porfolio', 'porfolio'),
+        ('ecommerce', 'ecommerce'),
+        ('education', 'education'),
+        ('health', 'health')
+    )
+    name = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    views = models.CharField(max_length=200, null=True)
+    pages = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Website(models.Model):
+    STATUS = (
+        ('Incomplete', 'Incomplete'),
+        ('Completed', 'Completed'),
+        ('Hosted', 'Hosted')
+    )
+    name = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+
+    
